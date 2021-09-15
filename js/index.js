a48a6832f9654be16aca6ceb52cf56975c6825c3
jhonvo <jhon26victor@gmail.com>
2021-09-15 18:41:33 -0400

    Updating content on the js and HTML file.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,24 @@
-function deleteElement (event){
-    alert ("Clicked on delete");
-    
+function deleteElement( event ){
+    console.log( event.target );
+    let entireElement = event.target.closest( 'li' );
+    console.log( entireElement );
+    entireElement.remove();
+}
+
+let deleteButtons = document.querySelectorAll( '.shopping-item-delete' );
+
+for( let i = 0; i < deleteButtons.length; i ++ ){
+    deleteButtons[i].addEventListener( "click", deleteElement );
+}
+
+function checkOrUncheck( element ){
+    let entireElement = element.closest( 'li' );
+    let currentItem = entireElement.querySelector( '.shopping-item' );
+    console.log( currentItem );
+    if( currentItem.classList.toString().includes( 'shopping-item__checked' ) ){
+        currentItem.classList.remove( 'shopping-item__checked' );
+    }
+    else{
+        currentItem.classList.add( 'shopping-item__checked' );
+    }
 }
